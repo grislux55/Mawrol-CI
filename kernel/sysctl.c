e894eb80ade257970df67da8ebd454a8d37ff707
grislux55 <jebchou.hg@gmail.com>
2019-09-03 11:33:50 +0800

    kernel/sysctl: Disable sched_walt_rotate_big_tasks
    Signed-off-by: grislux55 <jebchou.hg@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -368,6 +368,7 @@ static struct ctl_table kern_table[] = {
 		.extra1		= &neg_three,
 		.extra2		= &three,
 	},
+#if 0
 	{
 		.procname	= "sched_conservative_pl",
 		.data		= &sysctl_sched_conservative_pl,
@@ -395,6 +396,7 @@ static struct ctl_table kern_table[] = {
 		.extra1		= &zero,
 		.extra2		= &one,
 	},
+#endif
 	{
 		.procname	= "sched_min_task_util_for_boost",
 		.data		= &sysctl_sched_min_task_util_for_boost,
