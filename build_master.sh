f0b1aaaf362a22e2eeb62120de249e64eb88b79f
grislux55 <jebchou.hg@gmail.com>
2019-10-08 23:36:51 +0800

    kernel: change name to Mawrol
    Signed-off-by: grislux55 <jebchou.hg@gmail.com>

--- build_master.sh
+++ build_master.sh
@@ -5,17 +5,17 @@ if [[ "${1}" != "skip" ]] ; then
 	./build_kernel.sh stock "$@" || exit 1
 fi
 
-VERSION="$(cat version)-$(date +%F | sed s@-@@g)"
+VERSION="$(cat version)-$(date +'%y%m%d%H%M' | sed s@-@@g)"
 
 if [ -e boot.img ] ; then
-	rm arter97-kernel-$VERSION.zip 2>/dev/null
-	cp boot.img arter97-kernel-$VERSION.img
+	rm Mawrol-$VERSION.zip 2>/dev/null
+	cp boot.img Mawrol-$VERSION.img
 
 	# Pack AnyKernel3
 	rm -rf kernelzip
 	mkdir kernelzip
 	echo "
-kernel.string=arter97 kernel $(cat version) @ xda-developers
+kernel.string=Mawrol kernel $(cat version) @ xda-developers
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
@@ -31,10 +31,10 @@ ramdisk_compression=gzip
 	cp -rp ~/android/anykernel/* kernelzip/
 	find arch/arm64/boot/dts -name '*.dtb' -exec cat {} + > kernelzip/dtb
 	cd kernelzip/
-	7z a -mx9 arter97-kernel-$VERSION-tmp.zip *
-	7z a -mx0 arter97-kernel-$VERSION-tmp.zip ../arch/arm64/boot/Image.gz
-	zipalign -v 4 arter97-kernel-$VERSION-tmp.zip ../arter97-kernel-$VERSION.zip
-	rm arter97-kernel-$VERSION-tmp.zip
+	7z a -mx9 Mawrol-kernel-$VERSION-tmp.zip *
+	7z a -mx0 Mawrol-kernel-$VERSION-tmp.zip ../arch/arm64/boot/Image.gz
+	zipalign -v 4 Mawrol-kernel-$VERSION-tmp.zip ../Mawrol-kernel-$VERSION.zip
+	rm Mawrol-kernel-$VERSION-tmp.zip
 	cd ..
-	ls -al arter97-kernel-$VERSION.zip
+	ls -al Mawrol-kernel-$VERSION.zip
 fi
