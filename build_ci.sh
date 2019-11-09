fa9c2b3f5cad20e949b66192cce3cb6633bc3fa9
grislux55 <jebchou.hg@gmail.com>
2019-11-09 15:44:34 +0800

    build_script: add for CI support
    Signed-off-by: grislux55 <jebchou.hg@gmail.com>

---
+++ build_ci.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+jobs="-j$(nproc --all)"
+commit="$(cut -c-12 <<< "$(git rev-parse HEAD)")"
+
+echo "Start compiling! (Using $jobs flag)"
+./build_master.sh $jobs || exit
+
+git config --global user.name $GITNAME
+git config --global user.email $GITEMAIL
+
+git clone https://$GITID:$GITPWD@github.com/grislux55/kernel_release
+cd kernel_release
+mkdir -p $commit
+cp ../Mawrol-*.zip ./$commit
+
+git add . && git commit -m "build for $commit" -s
+git push https://$GITID:$GITPWD@github.com/grislux55/kernel_release HEAD:master
+
