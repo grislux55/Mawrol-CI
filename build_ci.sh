ae3be7613d4948e09402dd47c2f5e21e8a36db58
zrq8 <lhz@live.cn>
2019-12-14 18:22:07 +0800

    build_script: build_ci: update

--- build_ci.sh
+++ build_ci.sh
@@ -10,11 +10,11 @@ echo "Start compiling! (Using $jobs flag)"
 git config --global user.name $GITNAME
 git config --global user.email $GITEMAIL
 
-git clone https://$GITID:$GITPWD@github.com/grislux55/kernel_release
+git clone https://$GITID:$GITPWD@github.com/$GITID/kernel_release
 cd kernel_release
 mkdir -p "q/$(cat ../version)-$commit"
 cp ../Mawrol-*.zip "./q/$(cat ../version)-$commit"
 
 git add . && git commit -m "build for $commit" -s
-git push https://$GITID:$GITPWD@github.com/grislux55/kernel_release HEAD:master
+git push https://$GITID:$GITPWD@github.com/$GITID/kernel_release HEAD:master
 
