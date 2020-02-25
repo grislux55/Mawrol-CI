44f1c615568bad02a1cc008a3f7b963a6a0eee52
grislux55 <jebchou.hg@gmail.com>
2020-02-25 19:02:25 +0800

    build_system: Introduce toolchain
    Signed-off-by: grislux55 <jebchou.hg@gmail.com>

--- build_master.sh
+++ build_master.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+ANYKERNEL_PATH=./toolchain/anykernel
+
 if [[ "${1}" != "skip" ]] ; then
 	./build_clean.sh
 fi
@@ -17,25 +19,7 @@ if [ -e arch/arm64/boot/Image.gz ] ; then
 	# Pack AnyKernel3
 	rm -rf kernelzip
 	mkdir kernelzip
-	echo "
-kernel.string=arter97 kernel $(cat version) @ xda-developers
-do.devicecheck=1
-do.modules=0
-do.cleanup=1
-do.cleanuponabort=0
-device.name1=OnePlus7
-device.name2=OnePlus7Pro
-device.name3=OnePlus7T
-device.name4=OnePlus7TPro
-device.name5=guacamoleb
-device.name6=guacamole
-device.name7=hotdogb
-device.name8=hotdog
-block=/dev/block/bootdevice/by-name/boot
-is_slot_device=1
-ramdisk_compression=gzip
-" > kernelzip/props
-	cp -rp ~/android/anykernel/* kernelzip/
+	cp -rp $ANYKERNEL_PATH/* kernelzip/
 	find arch/arm64/boot/dts -name '*.dtb' -exec cat {} + > kernelzip/dtb
 	cd kernelzip/
 	7z a -mx9 arter97-kernel-$VERSION-tmp.zip *
