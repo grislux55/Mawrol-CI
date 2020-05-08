9b38b3189e94e65b33df297e3a570c9b87df7ef7
alk3pInjection <webmaster@raspii.tech>
2020-05-08 18:50:08 +0800

    build_script: Add toolchain support
    * Clang bes!

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 export KERNELDIR=`readlink -f .`
-export GCC64_PATH=./toolchain/aarch64-elf-gcc/bin/aarch64-elf-
-export GCC32_PATH=./toolchain/arm-eabi-gcc/bin/arm-eabi-
+export GCC64_PATH=./toolchain/proton-clang/bin/aarch64-linux-gnu-
+export GCC32_PATH=./toolchain/proton-clang/bin/arm-linux-gnueabi-
+export CLANG_PATH=./toolchain/proton-clang/bin/
+export PATH=${CLANG_PATH}:${PATH}
+export CLANG_TRIPLE=aarch64-linux-gnu-
+export CROSS_COMPILE=./toolchain/proton-clang/bin/aarch64-linux-gnu-
+export CROSS_COMPILE_ARM32=./toolchain/proton-clang/bin/arm-linux-gnueabi-
+export LD_LIBRARY_PATH=./toolchain/proton-clang/lib:$LD_LIBRARY_PATH
 
 echo "kerneldir = $KERNELDIR"
 
@@ -12,6 +18,6 @@ else
 	echo "Compiling kernel"
 	./generator ramdisk/init.qcom.post_boot.sh init/execprog.h
 	cp defconfig .config
-	make "$@" || exit 1
+	make CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip -j$(nproc --all) || exit 1
 	sed -i -e 's@"skip_initramfs"@"want_initramfs"@g' init/initramfs.c
 fi
